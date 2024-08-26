50bbfaf6fa63dfa00456d82f87fe0f3e2d9f6414
Jason Braun <jason.10@live.de>
2024-08-26 11:39:28 +0200

    added mobile class

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
+function isMobile(){
+    return /Android|iPhone/i.test(navigator.userAgent);
 
+}
+console.log(isMobile());
+
+window.onload = function(){
+    if (isMobile()){
+        document.body.classList.add("mobile")
+    }
+}
